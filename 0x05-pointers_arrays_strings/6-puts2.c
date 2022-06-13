a4ac9cd974b4f976d906f17f439e95f2de6ebdcb
marksikaundi <emmanuelmark117@gmail.com>
2022-06-13 16:37:13 -0700

    function that prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - print chracter of a string
+ * @str: character value
+ *Description: function to print every character of a string
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
